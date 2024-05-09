351b9edab300b02ca85eab9148678b977615a5f9
iam250502 <ilhammaulidan473@gmail.com>
2024-05-09 15:18:33 +0700

    categoryupdate

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,7 +3,33 @@
 @section('title', 'Category')
 
 @section('content')
-    <P>
-        kategori
-    </P>
+
+        <h1> category list </h1>
+
+        <div class="my-5">
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>NO.</th>
+                    <th>Name</th>
+                    <th>Action</th>
+                 </tr>
+            </thead>
+
+            <tbody>
+            @foreach ($categories as $item)
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+
+                <td>{{ $item->name }}</td>
+                
+                <td><a href="#">edit</a>
+                <a href="#">delete</a>
+            </td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+        </div>
 @endsection
